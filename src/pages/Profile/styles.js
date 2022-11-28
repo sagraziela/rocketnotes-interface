115b39c50922aa1d753cf2522ba82cc971df2194
Graziela Antunes <graziela.atns@gmail.com>
2022-11-28 10:36:26 -0300

    adjust profile height

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import theme from '../../styles/theme';
 
 export const Container = styled.div`
-    height: 100vh;
     display: flex;
     flex-direction: column;
 
