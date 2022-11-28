ccc12564977efaf1607fa18395a49db8eee7c0c1
Graziela Antunes <graziela.atns@gmail.com>
2022-11-28 09:53:42 -0300

    update base_URL from 'render'

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://rocketnotes-api12.herokuapp.com/"
+    baseURL: "https://rocketnotes-serever.onrender.com/"
 });
